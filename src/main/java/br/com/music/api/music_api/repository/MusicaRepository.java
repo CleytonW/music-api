82cf9ef1fd8a060d6a46a432da6b0cb343e12532
Cleyton Melo <cleytonw11@gmail.com>
2025-05-15 12:16:24 -0300

    Criando repository para conexão com banco de dados e assim realizar o CRUD

---
+++ src/main/java/br/com/music/api/music_api/repository/MusicaRepository.java
@@ -0,0 +1,7 @@
+package br.com.music.api.music_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MusicaRepository extends JpaRepository<Musica, Long> {
+
+}
