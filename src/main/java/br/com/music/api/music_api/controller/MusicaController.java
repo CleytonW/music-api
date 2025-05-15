84446a47f056c120f2cd1c8fea4fd11b3fe6d737
Cleyton Melo <cleytonw11@gmail.com>
2025-05-15 14:56:13 -0300

    Finalizando método get e testando a rota

--- src/main/java/br/com/music/api/music_api/controller/MusicaController.java
+++ src/main/java/br/com/music/api/music_api/controller/MusicaController.java
@@ -1,4 +1,24 @@
 package br.com.music.api.music_api.controller;
 
+import br.com.music.api.music_api.model.Musica;
+import br.com.music.api.music_api.repository.MusicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/musica")
 public class MusicaController {
+
+    @GetMapping
+    public List<Musica> listarMusica() {
+        return musicaRepository.findAll();
+    }
+
+
+    @Autowired
+    MusicaRepository musicaRepository;
 }
