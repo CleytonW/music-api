e846214000611bd9c1945c2450979177b2193595
Cleyton Melo <cleytonw11@gmail.com>
2025-05-15 12:18:29 -0300

    Corrigindo erro de importação na interface

--- src/main/java/br/com/music/api/music_api/repository/MusicaRepository.java
+++ src/main/java/br/com/music/api/music_api/repository/MusicaRepository.java
@@ -1,5 +1,6 @@
 package br.com.music.api.music_api.repository;
 
+import br.com.music.api.music_api.model.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MusicaRepository extends JpaRepository<Musica, Long> {
